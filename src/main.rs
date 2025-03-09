284de14a0895ad0b013925370ade659319bb9efc
Edward Vito <pizzacrustacean15@gmail.com>
2025-03-09 12:08:15 +0700

    Move functions to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use permutation_app::*;
 use std::collections::HashSet;
 use std::fmt;
 
@@ -22,49 +23,6 @@ fn main() {
 
     for (i, permutation) in permutations.iter().enumerate() {
         println!("Permutation {}: {:?}", i + 1, permutation);
-    }
-}
-
-fn create_population(nrof_males: i32, nrof_females: i32) -> Vec<String> {
-    let mut population: Vec<String> = Vec::new();
-
-    for i in 0..nrof_males {
-        population.push(format!("M{}", i + 1));
-    }
-
-    for i in 0..nrof_females {
-        population.push(format!("F{}", i + 1));
-    }
-
-    population
-}
-
-fn free_permute(
-    population: &[String],
-    nrof_chairs: usize,
-    permutation: &mut Vec<String>,
-    used: &mut HashSet<usize>,
-    permutations: &mut Vec<Vec<String>>,
-) {
-    // If the permutation is already full, add it to the list of permutations
-    if permutation.len() == nrof_chairs {
-        permutations.push(permutation.clone());
-        return;
-    }
-
-    for (i, person) in population.iter().enumerate() {
-        if !used.contains(&i) {
-            // Mark the person as used
-            used.insert(i);
-            permutation.push(person.clone());
-
-            // Recurse
-            free_permute(population, nrof_chairs, permutation, used, permutations);
-
-            // Backtrack
-            permutation.pop();
-            used.remove(&i);
-        }
     }
 }
 
